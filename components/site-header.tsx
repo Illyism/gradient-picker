aa333706ceec61cd96c005561f496db5376f3264
Ilias Ismanalijev <me@il.ly>
2023-08-05 13:05:47 +0200

    earn

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,18 +1,32 @@
 import Link from "next/link"
 
-import { siteConfig } from "@/config/site"
-import { buttonVariants } from "@/components/ui/button"
 import { Icons } from "@/components/icons"
 import { MainNav } from "@/components/main-nav"
 import { ThemeToggle } from "@/components/theme-toggle"
+import { Button, buttonVariants } from "@/components/ui/button"
+import { siteConfig } from "@/config/site"
+import { Gift } from "lucide-react"
 
 export function SiteHeader() {
   return (
-    <header className="bg-background sticky top-0 z-40 w-full border-b">
+    <header className="sticky top-0 z-40 w-full border-b bg-background">
       <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
         <MainNav items={siteConfig.mainNav} />
         <div className="flex flex-1 items-center justify-end space-x-4">
           <nav className="flex items-center space-x-1">
+          <Link
+              href="https://gradient.page/affiliate"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <Button
+                size="sm"
+                className="bg-gradient-to-r from-[#D70652] to-[#FF025E] font-bold text-white "
+              >
+                <Gift className="mr-2 h-5 w-5" />
+                Earn 20%
+              </Button>
+            </Link>
             <Link
               href={siteConfig.links.github}
               target="_blank"
