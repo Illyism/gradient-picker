3de728f91bd31b9b0ca26494817a66c93fc4efad
Ilias Ismanalijev <me@il.ly>
2023-08-06 09:50:59 +0200

    mobile

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -15,7 +15,7 @@ export function MainNav({ items }: MainNavProps) {
     <div className="flex gap-6 md:gap-10">
       <Link href="/" className="flex items-center space-x-2">
         <Icons.logo className="h-6 w-6" />
-        <span className="inline-block font-bold">{siteConfig.name}</span>
+        <span className="hidden font-bold sm:inline-block">{siteConfig.name}</span>
       </Link>
       {items?.length ? (
         <nav className="flex gap-6">
